4c226dd2b01653961db6b9c0aa81e89f66937291
Mehmet Pekdemir <mehmetpekdemir06@hotmail.com>
2020-03-16 05:36:46 +0300

    Model sınıflarım eklendi.

---
+++ backend/src/main/com/mehmetpekdemir/petclinic/model/Owner.java
@@ -0,0 +1,70 @@
+package com.mehmetpekdemir.petclinic.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "owner")
+@XmlRootElement
+public class Owner {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "first_name", length = 50)
+	private String firstName;
+
+	@Column(name = "last_name", length = 50)
+	private String lastName;
+
+	@OneToMany(mappedBy = "owner")
+	private Set<Pet> pets = new HashSet<Pet>();
+
+	public Long getId() {
+		return id;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@XmlTransient
+	@JsonIgnore
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
+
+	@Override
+	public String toString() {
+		return "Owner [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+
+}
---
+++ backend/src/main/com/mehmetpekdemir/petclinic/model/Pet.java
@@ -0,0 +1,64 @@
+package com.mehmetpekdemir.petclinic.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pet")
+public class Pet {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name", length = 50)
+	private String name;
+
+	@Column(name = "birth_date")
+	private Date birthDate;
+
+	@ManyToOne
+	@JoinColumn(name = "owner_id")
+	private Owner owner;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Owner getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Owner owner) {
+		this.owner = owner;
+	}
+
+	@Override
+	public String toString() {
+		return "Pet [id=" + id + ", name=" + name + ", birthDate=" + birthDate + ", owner=" + owner + "]";
+	}
+
+}
