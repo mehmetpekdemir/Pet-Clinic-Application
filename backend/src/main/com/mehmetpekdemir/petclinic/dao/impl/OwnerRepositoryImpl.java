4605f91de2078982be5471e6db625f583fad455a
Mehmet Pekdemir <mehmetpekdemir06@hotmail.com>
2020-03-16 05:40:45 +0300

    Owner ve pet için veri erişim katmanım eklendi ve soyutlama islemi yapıldı.

---
+++ backend/src/main/com/mehmetpekdemir/petclinic/dao/impl/OwnerRepositoryImpl.java
@@ -0,0 +1,50 @@
+package com.mehmetpekdemir.petclinic.dao.impl;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import com.mehmetpekdemir.petclinic.dao.OwnerRepository;
+import com.mehmetpekdemir.petclinic.model.Owner;
+
+@Repository("ownerRepository")
+public class OwnerRepositoryImpl implements OwnerRepository {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Override
+	public List<Owner> findAll() {
+		return entityManager.createQuery("from Owner", Owner.class).getResultList();
+	}
+
+	@Override
+	public Owner findById(Long id) {
+		return entityManager.find(Owner.class, id);
+	}
+
+	@Override
+	public List<Owner> findByLastName(String lastName) {
+		return entityManager.createQuery("from Owner where lastName = :lastName", Owner.class)
+				.setParameter("lastName", lastName).getResultList();
+	}
+
+	@Override
+	public void createOwner(Owner owner) {
+		entityManager.persist(owner);
+	}
+
+	@Override
+	public Owner updateOwner(Owner owner) {
+		return entityManager.merge(owner);
+	}
+
+	@Override
+	public void deleteOwner(Long id) {
+		entityManager.remove(entityManager.getReference(Owner.class, id));
+	}
+
+}
---
+++ backend/src/main/com/mehmetpekdemir/petclinic/dao/impl/PetRepositoryImpl.java
@@ -0,0 +1,51 @@
+package com.mehmetpekdemir.petclinic.dao.impl;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import com.mehmetpekdemir.petclinic.dao.PetRepository;
+import com.mehmetpekdemir.petclinic.model.Pet;
+
+@Repository("petRepository")
+public class PetRepositoryImpl implements PetRepository {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Override
+	public Pet findById(Long id) {
+		return entityManager.find(Pet.class, id);
+	}
+
+	@Override
+	public List<Pet> findByOwnerId(Long ownerId) {
+		return entityManager.createQuery("from Pet where owner.id = :ownerId", Pet.class)
+				.setParameter("ownerId", ownerId).getResultList();
+	}
+
+	@Override
+	public void createPet(Pet pet) {
+		entityManager.persist(pet);
+	}
+
+	@Override
+	public Pet updatePet(Pet pet) {
+		return entityManager.merge(pet);
+	}
+
+	@Override
+	public void deletePet(Long id) {
+		entityManager.remove(entityManager.getReference(Pet.class, id));
+	}
+
+	@Override
+	public void deleteByOwnerId(Long id) {
+		entityManager.createQuery("delete from Pet where owner.id = :ownerId").setParameter("ownerId", id)
+				.executeUpdate();
+	}
+
+}
