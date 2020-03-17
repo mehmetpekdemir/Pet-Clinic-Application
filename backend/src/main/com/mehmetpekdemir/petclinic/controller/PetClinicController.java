3f19bd60b7aa8505268210654d329602a7fc79f0
Mehmet Pekdemir <mehmetpekdemir06@hotmail.com>
2020-03-17 19:10:11 +0300

    Controller güncellendi.

--- backend/src/main/com/mehmetpekdemir/petclinic/controller/PetClinicController.java
+++ backend/src/main/com/mehmetpekdemir/petclinic/controller/PetClinicController.java
@@ -4,9 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * 
+ * @author MEHMET PEKDEMIR
+ *
+ */
 @Controller
 public class PetClinicController {
 
+	/**
+	 * Anasayfaya yönlenmemizi sağlar.
+	 * 
+	 * @return modelAndView
+	 */
 	@RequestMapping(value = { "/", "/index.html" })
 	public ModelAndView index() {
 		ModelAndView modelAndView = new ModelAndView();
