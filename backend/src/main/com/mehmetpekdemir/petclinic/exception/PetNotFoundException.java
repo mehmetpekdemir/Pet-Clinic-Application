5d225deb27d612420a71738a3b043d50cd63f37d
Mehmet Pekdemir <mehmetpekdemir06@hotmail.com>
2020-03-17 19:10:58 +0300

    Exceptionlar güncellendi.

--- backend/src/main/com/mehmetpekdemir/petclinic/exception/PetNotFoundException.java
+++ backend/src/main/com/mehmetpekdemir/petclinic/exception/PetNotFoundException.java
@@ -4,17 +4,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
- * Owner bulunmaması durumda (404) kodunu döneceğim classım
+ * Pet bulunmaması durumda (404) kodunu döneceğim classım
  * 
  * @author MEHMET
  *
  */
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class OwnerNotFoundException extends RuntimeException {
+public class PetNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
-	public OwnerNotFoundException(String message) {
+	public PetNotFoundException(String message) {
 		super(message);
 	}
 
